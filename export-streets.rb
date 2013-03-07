4712cc303bbad0d50fac7a407b3bf97932758f5a
Nicolas Marchildon <nicolas@marchildon.net>
2013-03-06 23:49:26 -0500

    export-streets.rb updated for new ScrapeCache API

--- export-streets.rb
+++ export-streets.rb
@@ -4,17 +4,18 @@ require 'rubygems'
 require 'mechanize'
 require './lib/cache.rb'
 
+cache = ScrapeCache.new
 File.open("streets.csv", 'w:UTF-8') do |csv|
   [('aa'..'zz'),(0..9)].each do |ranges|
     ranges.each do |search_term|
-      streets_body = ScrapeCache::get("street_search", search_term)
+      streets_body = cache.get("street_search", search_term)
       if (!streets_body.nil?)
         puts "CACHE search: #{search_term}"
         streets_page = Nokogiri::HTML::Document.parse(streets_body, encoding='UTF-8')
         streets_page.css('/html/body/div/div/div/p[6]/select/option').each do |street_option|
           street_id = street_option.attribute('value').value
           street_name = street_option.content.gsub(/\s+/, " ").gsub("'", "''")
-          street_body = ScrapeCache::get("street", street_id)
+          street_body = cache.get("street", street_id)
           if (!street_body.nil?)
             puts "CACHE street: " + street_id + " (" + street_name + ")"
             street_page = Nokogiri::HTML::Document.parse(street_body)
